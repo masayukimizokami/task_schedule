b05b7edc35d2026264e18365536c948638fc309e
溝上正幸 <mizokamimasayuki@mizokamimasayukinoMacBook-Air.local>
2021-11-02 05:37:55 +0900

    新規作成エラー改善中

---
+++ app/controllers/schedules_controller.rb
@@ -0,0 +1,52 @@
+class SchedulesController < ApplicationController
+  def index
+    @scdls = Scdl.all
+  end
+
+  def new
+    @scdls = Scdl.all
+    @scdl = Scdl.new
+  end
+
+  def create
+    @scdl = Scdl.new(params.require(:scdl).permit(:title, :start_day, :end_day, :all_day, :memo))
+    if @scdl.save
+      flash[:success]= "スケジュールを登録しました"
+      redirect_to :schedules
+    else
+      flash[:delete]= "スケジュールを登録できませんでした"
+      render "new"
+    end
+    @scdls = Scdl.all
+  end
+  
+  def show
+    @scdl = Scdl.find(params[:id])
+    @scdls = Scdl.all
+  end
+  
+  def edit
+    @scdl = Scdl.find(params[:id])
+    @scdls = Scdl.all
+  end
+  
+  def update
+    @scdl = Scdl.find(params[:id])
+    if @scdl.update(params.require(:scdl).permit(:title, :start_day, :end_aday, :all_day, :memo))
+      flash[:success] = "スケジュールを更新しました"
+      redirect_to :scdls
+    else
+      flash[:delete]= "スケジュールを更新できませんでした"
+      render "edit"
+    end
+    @scdls = Scdl.all
+  end
+  
+  def destroy
+    @scdl = Scdl.find(params[:id])
+    @scdl.destroy
+    flash[:delete]= "スケジュールを削除しました"
+    redirect_to :scdls
+  end
+
+end
